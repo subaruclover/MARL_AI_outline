cf7be5ef8a29476a2b2c1d5c39e9ce9b19bfbb98
SUBARU <subaru6725@hotmail.com>
2022-10-18 14:09:33 +0900

    change path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,16 +34,22 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
+source_suffix = ['.rst', '.md']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here
-html_static_path = ['marker']
+html_static_path = ['_static']
 
-html_logo = 'marker/images/logo.png'
+html_logo = '_static/images/logo.png'
 
 
 # -- Options for EPUB output
